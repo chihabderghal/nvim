e0b421fbd81fb863ee4c66f8ba2371da52853586
Chihab Derghal <chihabderghal1@gmail.com>
2024-09-03 01:35:58 +0100

    Add bindings.lua

---
+++ lua/chihabderghal/bindings.lua
@@ -0,0 +1,44 @@
+-- Diagnostic bindings
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+
+-- Exit terminal mode in the builtin terminal binding
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>")
+
+-- Faster split navigation bindings
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
+
+-- Zen mode
+vim.keymap.set("n", "<leader>zz", function()
+	require("zen-mode").setup({
+		window = {
+			width = 90,
+			options = {},
+		},
+	})
+	require("zen-mode").toggle()
+	vim.wo.wrap = false
+	vim.wo.number = true
+	vim.wo.rnu = true
+end)
+
+vim.keymap.set("n", "<leader>zZ", function()
+	require("zen-mode").setup({
+		window = {
+			width = 80,
+			options = {},
+		},
+	})
+	require("zen-mode").toggle()
+	vim.wo.wrap = false
+	vim.wo.number = false
+	vim.wo.rnu = false
+	vim.opt.colorcolumn = "0"
+end)
+
+-- vim: ts=2 sts=2 sw=2 et

b2ab4b4608604bdfe5b4f5da76f6ce150eb3c365
Chihab Derghal <chihabderghal1@gmail.com>
2024-09-03 01:36:48 +0100

    Add options.lua

---
+++ lua/chihabderghal/options.lua
@@ -0,0 +1,24 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.have_nerd_font = false
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.showmode = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.inccommand = "split"
+vim.opt.hlsearch = true
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.opt.cursorline = false
+vim.opt.scrolloff = 10
+vim.opt.list = false
+vim.opt.listchars = { trail = "·", tab = "» ", nbsp = "␣" }

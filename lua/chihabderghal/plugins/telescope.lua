b15f6698376640fcc6b6b8890a113951b2b61787
Chihab Derghal <chihabderghal1@gmail.com>
2024-09-03 01:55:27 +0100

    Update telescope bindings

--- lua/chihabderghal/plugins/telescope.lua
+++ lua/chihabderghal/plugins/telescope.lua
@@ -41,14 +41,13 @@ return {
 		vim.keymap.set("n", "<leader>sh", builtin.help_tags)
 		vim.keymap.set("n", "<leader>sk", builtin.keymaps)
 		vim.keymap.set("n", "<leader>pf", builtin.git_files)
-		vim.keymap.set("n", "<leader>ff", builtin.find_files)
+		vim.keymap.set("n", "<leader><leader>", builtin.find_files)
 		vim.keymap.set("n", "<leader>ss", builtin.builtin)
 		vim.keymap.set("n", "<leader>sw", builtin.grep_string)
 		vim.keymap.set("n", "<leader>sg", builtin.live_grep)
 		vim.keymap.set("n", "<leader>sd", builtin.diagnostics)
 		vim.keymap.set("n", "<leader>sr", builtin.resume)
 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles)
-		vim.keymap.set("n", "<leader><leader>", builtin.buffers)
 
 		-- Binding to search in the current buffer
 		vim.keymap.set("n", "<leader>/", function()
